eb78195c4e4cecc79c1157a22aa458cad2f6844e
Edgar-Gegorio <egmez124@gmail.com>
2021-10-18 22:21:43 -0500

    cambios-1

--- src/App.js
+++ src/App.js
@@ -1,43 +1,46 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 import WeatherBox from './components/weatherbox';
+import axios from 'axios';
 
 function App() {
-  const [img, setImg] = useState(0)
-  const [location, setLocation] = useState('')
-  const [condition, setCondition] = useState('')
-  const [wind, setWind] = useState('')
-  const [clouds, setClouds] = useState('')
-  const [pressure, setPressure] = useState('')
-  const [temp, setTemp] = useState('')
-  const [tempF, setTempF] = useState('')
-  useEffect(()=>{
-    const getData = async()=>{
-      const resIp = await fetch(`https://api.ipify.org?format=json`).then(res => res.json())
-      const ip = resIp.ip
-      const url = `https://api.weatherapi.com/v1/current.json?key=640ff40b447a40a38ae163312210507&q=${ip}`
-      const response = await fetch(url).then(res => res.json())
-      const data = response.current
-      setImg(data.condition.icon)
-      setLocation(response.location.tz_id)
-      setCondition(data.condition.text)
-      setWind(data.wind_kph)
-      setClouds(data.cloud)
-      setPressure(data.pressure_mb)
-      setTemp(data.temp_c)
-      setTempF(data.temp_f)
-    }
-    getData()
-  }, [])
+  const [current, setCurrent] = useState([]);
+  const [location, setLocation] = useState([]);
+  const [condition, setCondition] = useState([]);
 
-  const changeTemp = () => {
-    if (temp) {
-      setTemp(tempF)
-    }
+  useEffect(()=>{
+    
+      const getData = async()=>{
+        const resIp = await fetch(`https://api.ipify.org?format=json`).then(res=>res.json());
+        const ip = resIp.ip
+        const url = `https://api.weatherapi.com/v1/current.json?key=640ff40b447a40a38ae163312210507&q=${ip}`
+        const response = await axios.get(url);
+        console.log(response.data.current);
+        setCurrent(response.data.current);
+        setLocation(response.data.location);
+        setCondition(response.data.current.condition);
+      }
+      getData();
+    
+  },[])
+  
+  const handleSearch = async (country)=>{
+    const url = `http://api.weatherapi.com/v1/current.json?key=640ff40b447a40a38ae163312210507&q=${country}&aqi=no`;
+    const response = await axios.get(url);
+    console.log(response);
+    setCurrent(response.data.current);
+    setLocation(response.data.location);
+    setCondition(response.data.current.condition);
   }
+  
   return (
     <div className="App">
-       <WeatherBox img={img} location={location} condition={condition} wind={wind} clouds={clouds} pressure={pressure} temp={temp} changeTemp={changeTemp}/>
+        <WeatherBox 
+        current={current}
+        location={location}
+        condition={condition}
+        handleSearch={handleSearch}
+        />
     </div>
   );
 }
--- src/components/weatherbox.js
+++ src/components/weatherbox.js
@@ -1,21 +1,37 @@
-const WeatherBox = ({img, location, condition, wind, clouds, pressure, temp, changeTemp}) => {
+import { useState } from "react"
+
+const WeatherBox = ({current, location, condition, handleSearch}) => {
+    const [temp, setTemp] = useState('');
+    const [value, setValue] = useState('');
+
+    const changeTemp = ()=>{
+        if(temp){
+            setTemp(false);
+        }else{
+            setTemp(true)
+        }
+    }
     return (
-        <div class="card mb-3" >
-            <div class="row g-0">
-                <div class="col-md-8">
-                    <div class="card-body">
-                        <h3 class="card-title">Weathe APP</h3>
-                        <img src={img} alt={location}></img>
-                        <p class="card-text">{location}</p>
-                        <p class="card-text">Temperatura: {temp}</p>
-                        <p class="card-text"><small class="text-muted">{condition}</small></p>
-                        <p class="card-text"><small class="text-muted">Wind speed kph: {wind} </small></p>
-                        <p class="card-text"><small class="text-muted">Clouds: {clouds}%</small></p>
-                        <p class="card-text"><small class="text-muted">Pressure (mb): {pressure} </small></p>
-                        <button type="button" class="btn btn-warning" onClick={changeTemp}>Cambiar a grados Fahrenheit</button>
-                    </div>
-                </div>
+        <div className="card-body">
+            <div className="header">
+                <h1>Weather APP</h1>
+                <input className="input" placeholder="introduce un pais..." onChange={(e)=>{setValue(e.target.value)}} value={value}/>
+                <button onClick={()=>{handleSearch(value)}}>Search</button>
             </div>
+            <div className="card-temp">
+                <img src={condition.icon} alt={'condition.icon'} className="icon"></img>
+                <p class="card-text">{location.country}</p>
+                {temp?<p>{current.temp_c}ºC</p>:<p>{current.temp_f}ºF</p>}
+                <button className="boton" onClick={changeTemp}>{temp?'Cambiar a farenheit':'Cambiar a centigrados'}</button>
+            </div>
+            <div className="card-data">
+                <h2 className="conditions-title">Conditions</h2>
+                <p>{condition.text}</p>
+                <p>Wind speed kph: {current.wind_mph}</p>
+                <p>Clouds: {current.cloud}%</p>
+                <p>Pressure (mb): {current.pressure_in}</p>
+            </div>
+            {/* <div></div> */}
         </div>
     )
 }
